299bb9f2c4e817c6c5cb50bd6de1d578bfdcdf9c
Liz Antoinette Anguren <31739093+LizAntoinette@users.noreply.github.com>
2021-04-27 16:04:37 +0800

    adding queen...

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -7,40 +7,50 @@
             <td
               v-for="elements in items"
               :key="elements.id"
+              :id = elements.name
+              ref="elements.id"
               :class="elements.class"
               height="95px"
               width="90px"
-            ></td>
+            >
+
+              <!-- <img src="../assets/queen.png"  ref="elements.id" alt="Queen"> -->
+            </td>
           </tr>
         </tbody>
       </table>
     </div>
     <div class="mt-3">
-     
-        <b-button class="mr-2" size="sm">Reset</b-button>
-        <b-button class="mr-2" size="sm">Start</b-button>
-        
-        <b-button class="mr-2"size="sm">Previous</b-button>
-        <b-button class="mr-2"size="sm">Next</b-button>
-  
+      <b-button class="mr-2" size="sm" :disabled="btnReset">Reset</b-button>
+      <b-button class="mr-2" size="sm" @click="startforQueen" :disabled="btnStart">Start</b-button>
+
+      <b-button class="mr-2" size="sm" :disabled="btnPrev">Previous</b-button>
+      <b-button class="mr-2" size="sm"  :disabled="btnNext">Next</b-button>
     </div>
   </div>
 </template>
 <script>
-import {fourQueen} from '../algorithms/fourQueens';
+import { fourQueen } from "../algorithms/fourQueens";
 
 export default {
   data() {
     return {
       chessboard: null,
       animateArray: [],
-      btnNext: false, 
-      btnPrev: false
+      btnNext: true,
+      btnPrev: true,
+      btnReset: true,
+      btnStart: false,
     };
   },
   created: function () {
     this.createChessBoard();
-    
+  },
+  mounted:function(){
+      console.log(this.$refs.td11)
+    // startforQueen(){
+      
+    // }
   },
   methods: {
     createChessBoard() {
@@ -49,26 +59,35 @@ export default {
         let currentRow = [];
         for (let j = 1; j <= 4; j++) {
           if ((j % 2 == 0 && i % 2 == 0) || (j % 2 != 0 && i % 2 != 0)) {
-            currentRow.push({ id: "" + i + "" + j, class: "black" });
+            currentRow.push({ name: "td" + i + "" + j, class: "black" });
           } else {
-            currentRow.push({ id: "" + i + "" + j, class: "white" });
+            currentRow.push({ name: "td" + i + "" + j, class: "white" });
           }
         }
         this.chessboard.push(currentRow);
       }
-      console.log(this.chessboard);
+     
     },
-    startforQueen(){
+   
+    startforQueen() {
       this.animateArray = fourQueen();
-      this.btnNext = true;
-      this.btnPrev = true;
-    }, 
-    nextfourQueen(){
+      this.btnNext = false;
+      this.btnStart = true;
+      console.log(this.$refs.td11)
+      var img = document.createElement("img");
+      img.alt="Queen";
+      img.src = "https://i.ibb.co/kMBQN3w/queen.png";
+      var src = document.getElementById("td11");
+      src.appendChild(img);
+    
+      
+    },
+    nextfourQueen() {
 
     },
-    prevfourQueen(){
-
-    }
+    prevfourQueen() {},
+    resetChessBoard() {},
+ 
   },
 };
 </script>
